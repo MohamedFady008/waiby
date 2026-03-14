e9078037e854bb6ad8ca91bc5aa566faf0803c8e
MohamedFady008 <eng.mohamedfadyfouad@gmail.com>
2026-03-14 08:14:46 +0200

    Fix web bootstrap without local Firebase env

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -2,6 +2,29 @@ import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
+const _firebaseProjectId = 'waiby-89732';
+const _firebaseDatabaseUrl = 'https://waiby-89732-default-rtdb.firebaseio.com';
+const _firebaseStorageBucket = 'waiby-89732.firebasestorage.app';
+const _firebaseMessagingSenderId = '686865131708';
+
+const _firebaseWebApiKey = 'AIzaSyB4NHP7bOIXh_QvS84JZRwtqBIde_A_WsI';
+const _firebaseWebAppId = '1:686865131708:web:f121c754016723d44848b9';
+const _firebaseWebAuthDomain = 'waiby-89732.firebaseapp.com';
+const _firebaseWebMeasurementId = 'G-V195160D2X';
+
+const _firebaseAndroidApiKey = 'AIzaSyA73uqaCR_yD_pzMP04FjAaJm1WCK4H89k';
+const _firebaseAndroidAppId = '1:686865131708:android:14187e1eb86903614848b9';
+
+const _firebaseIosApiKey = 'AIzaSyCY5jM4n-PUulpedBoMC7Q4qJlFAtq0mzk';
+const _firebaseIosAppId = '1:686865131708:ios:8d23694559efd3754848b9';
+const _firebaseIosClientId =
+    '686865131708-d59q0kirn2ss47d38e4ofqk2m7sb3jd3.apps.googleusercontent.com';
+const _firebaseIosBundleId = 'com.example.waiby';
+
+const _firebaseWindowsAppId = '1:686865131708:web:467c8fa9638e4ea34848b9';
+const _firebaseWindowsAuthDomain = 'waiby-89732.firebaseapp.com';
+const _firebaseWindowsMeasurementId = 'G-0W636N58LV';
+
 /// Default [FirebaseOptions] for use with your Firebase apps.
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
@@ -84,56 +107,170 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_WEB_API_KEY'),
-    appId: String.fromEnvironment('FIREBASE_WEB_APP_ID'),
-    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID'),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID'),
-    authDomain: String.fromEnvironment('FIREBASE_WEB_AUTH_DOMAIN'),
-    databaseURL: String.fromEnvironment('FIREBASE_DATABASE_URL'),
-    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET'),
-    measurementId: String.fromEnvironment('FIREBASE_WEB_MEASUREMENT_ID'),
+    apiKey: String.fromEnvironment(
+      'FIREBASE_WEB_API_KEY',
+      defaultValue: _firebaseWebApiKey,
+    ),
+    appId: String.fromEnvironment(
+      'FIREBASE_WEB_APP_ID',
+      defaultValue: _firebaseWebAppId,
+    ),
+    messagingSenderId: String.fromEnvironment(
+      'FIREBASE_MESSAGING_SENDER_ID',
+      defaultValue: _firebaseMessagingSenderId,
+    ),
+    projectId: String.fromEnvironment(
+      'FIREBASE_PROJECT_ID',
+      defaultValue: _firebaseProjectId,
+    ),
+    authDomain: String.fromEnvironment(
+      'FIREBASE_WEB_AUTH_DOMAIN',
+      defaultValue: _firebaseWebAuthDomain,
+    ),
+    databaseURL: String.fromEnvironment(
+      'FIREBASE_DATABASE_URL',
+      defaultValue: _firebaseDatabaseUrl,
+    ),
+    storageBucket: String.fromEnvironment(
+      'FIREBASE_STORAGE_BUCKET',
+      defaultValue: _firebaseStorageBucket,
+    ),
+    measurementId: String.fromEnvironment(
+      'FIREBASE_WEB_MEASUREMENT_ID',
+      defaultValue: _firebaseWebMeasurementId,
+    ),
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_ANDROID_API_KEY'),
-    appId: String.fromEnvironment('FIREBASE_ANDROID_APP_ID'),
-    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID'),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID'),
-    databaseURL: String.fromEnvironment('FIREBASE_DATABASE_URL'),
-    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET'),
+    apiKey: String.fromEnvironment(
+      'FIREBASE_ANDROID_API_KEY',
+      defaultValue: _firebaseAndroidApiKey,
+    ),
+    appId: String.fromEnvironment(
+      'FIREBASE_ANDROID_APP_ID',
+      defaultValue: _firebaseAndroidAppId,
+    ),
+    messagingSenderId: String.fromEnvironment(
+      'FIREBASE_MESSAGING_SENDER_ID',
+      defaultValue: _firebaseMessagingSenderId,
+    ),
+    projectId: String.fromEnvironment(
+      'FIREBASE_PROJECT_ID',
+      defaultValue: _firebaseProjectId,
+    ),
+    databaseURL: String.fromEnvironment(
+      'FIREBASE_DATABASE_URL',
+      defaultValue: _firebaseDatabaseUrl,
+    ),
+    storageBucket: String.fromEnvironment(
+      'FIREBASE_STORAGE_BUCKET',
+      defaultValue: _firebaseStorageBucket,
+    ),
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_IOS_API_KEY'),
-    appId: String.fromEnvironment('FIREBASE_IOS_APP_ID'),
-    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID'),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID'),
-    databaseURL: String.fromEnvironment('FIREBASE_DATABASE_URL'),
-    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET'),
-    iosClientId: String.fromEnvironment('FIREBASE_IOS_CLIENT_ID'),
-    iosBundleId: String.fromEnvironment('FIREBASE_IOS_BUNDLE_ID'),
+    apiKey: String.fromEnvironment(
+      'FIREBASE_IOS_API_KEY',
+      defaultValue: _firebaseIosApiKey,
+    ),
+    appId: String.fromEnvironment(
+      'FIREBASE_IOS_APP_ID',
+      defaultValue: _firebaseIosAppId,
+    ),
+    messagingSenderId: String.fromEnvironment(
+      'FIREBASE_MESSAGING_SENDER_ID',
+      defaultValue: _firebaseMessagingSenderId,
+    ),
+    projectId: String.fromEnvironment(
+      'FIREBASE_PROJECT_ID',
+      defaultValue: _firebaseProjectId,
+    ),
+    databaseURL: String.fromEnvironment(
+      'FIREBASE_DATABASE_URL',
+      defaultValue: _firebaseDatabaseUrl,
+    ),
+    storageBucket: String.fromEnvironment(
+      'FIREBASE_STORAGE_BUCKET',
+      defaultValue: _firebaseStorageBucket,
+    ),
+    iosClientId: String.fromEnvironment(
+      'FIREBASE_IOS_CLIENT_ID',
+      defaultValue: _firebaseIosClientId,
+    ),
+    iosBundleId: String.fromEnvironment(
+      'FIREBASE_IOS_BUNDLE_ID',
+      defaultValue: _firebaseIosBundleId,
+    ),
   );
 
   static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_MACOS_API_KEY'),
-    appId: String.fromEnvironment('FIREBASE_MACOS_APP_ID'),
-    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID'),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID'),
-    databaseURL: String.fromEnvironment('FIREBASE_DATABASE_URL'),
-    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET'),
-    iosClientId: String.fromEnvironment('FIREBASE_MACOS_IOS_CLIENT_ID'),
-    iosBundleId: String.fromEnvironment('FIREBASE_MACOS_BUNDLE_ID'),
+    apiKey: String.fromEnvironment(
+      'FIREBASE_MACOS_API_KEY',
+      defaultValue: _firebaseIosApiKey,
+    ),
+    appId: String.fromEnvironment(
+      'FIREBASE_MACOS_APP_ID',
+      defaultValue: _firebaseIosAppId,
+    ),
+    messagingSenderId: String.fromEnvironment(
+      'FIREBASE_MESSAGING_SENDER_ID',
+      defaultValue: _firebaseMessagingSenderId,
+    ),
+    projectId: String.fromEnvironment(
+      'FIREBASE_PROJECT_ID',
+      defaultValue: _firebaseProjectId,
+    ),
+    databaseURL: String.fromEnvironment(
+      'FIREBASE_DATABASE_URL',
+      defaultValue: _firebaseDatabaseUrl,
+    ),
+    storageBucket: String.fromEnvironment(
+      'FIREBASE_STORAGE_BUCKET',
+      defaultValue: _firebaseStorageBucket,
+    ),
+    iosClientId: String.fromEnvironment(
+      'FIREBASE_MACOS_IOS_CLIENT_ID',
+      defaultValue: _firebaseIosClientId,
+    ),
+    iosBundleId: String.fromEnvironment(
+      'FIREBASE_MACOS_BUNDLE_ID',
+      defaultValue: _firebaseIosBundleId,
+    ),
   );
 
   static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_WINDOWS_API_KEY'),
-    appId: String.fromEnvironment('FIREBASE_WINDOWS_APP_ID'),
-    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID'),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID'),
-    authDomain: String.fromEnvironment('FIREBASE_WINDOWS_AUTH_DOMAIN'),
-    databaseURL: String.fromEnvironment('FIREBASE_DATABASE_URL'),
-    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET'),
-    measurementId: String.fromEnvironment('FIREBASE_WINDOWS_MEASUREMENT_ID'),
+    apiKey: String.fromEnvironment(
+      'FIREBASE_WINDOWS_API_KEY',
+      defaultValue: _firebaseWebApiKey,
+    ),
+    appId: String.fromEnvironment(
+      'FIREBASE_WINDOWS_APP_ID',
+      defaultValue: _firebaseWindowsAppId,
+    ),
+    messagingSenderId: String.fromEnvironment(
+      'FIREBASE_MESSAGING_SENDER_ID',
+      defaultValue: _firebaseMessagingSenderId,
+    ),
+    projectId: String.fromEnvironment(
+      'FIREBASE_PROJECT_ID',
+      defaultValue: _firebaseProjectId,
+    ),
+    authDomain: String.fromEnvironment(
+      'FIREBASE_WINDOWS_AUTH_DOMAIN',
+      defaultValue: _firebaseWindowsAuthDomain,
+    ),
+    databaseURL: String.fromEnvironment(
+      'FIREBASE_DATABASE_URL',
+      defaultValue: _firebaseDatabaseUrl,
+    ),
+    storageBucket: String.fromEnvironment(
+      'FIREBASE_STORAGE_BUCKET',
+      defaultValue: _firebaseStorageBucket,
+    ),
+    measurementId: String.fromEnvironment(
+      'FIREBASE_WINDOWS_MEASUREMENT_ID',
+      defaultValue: _firebaseWindowsMeasurementId,
+    ),
   );
 
   static void _assertRequired(Map<String, String> requiredDefines) {
--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,32 @@ import 'core/web/record_web_plugin_config.dart';
 import 'core/web/url_strategy_config.dart';
 import 'firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  configureWebUrlStrategy();
-  ensureAudioplayersWebPluginRegistered();
-  ensureRecordWebPluginRegistered();
+  try {
+    configureWebUrlStrategy();
+    ensureAudioplayersWebPluginRegistered();
+    ensureRecordWebPluginRegistered();
 
-  await ensureFirebaseWebModulesLoaded();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  Get.put(AuthController(), permanent: true);
-  Get.put(ChatController(), permanent: true);
-  Get.lazyPut(() => CreatorFormController(), fenix: true);
-  Get.lazyPut(() => HomeController(), fenix: true);
-  runApp(const MyApp());
+    await ensureFirebaseWebModulesLoaded();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    Get.put(AuthController(), permanent: true);
+    Get.put(ChatController(), permanent: true);
+    Get.lazyPut(() => CreatorFormController(), fenix: true);
+    Get.lazyPut(() => HomeController(), fenix: true);
+    runApp(const MyApp());
+  } catch (error, stackTrace) {
+    FlutterError.reportError(
+      FlutterErrorDetails(
+        exception: error,
+        stack: stackTrace,
+        library: 'waiby bootstrap',
+      ),
+    );
+    runApp(BootstrapFailureApp(error: error.toString()));
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -46,6 +59,83 @@ class MyApp extends StatelessWidget {
       routeInformationParser: router.routeInformationParser,
       routeInformationProvider: router.routeInformationProvider,
       theme: ThemeData(useMaterial3: true),
+    );
+  }
+}
+
+class BootstrapFailureApp extends StatelessWidget {
+  const BootstrapFailureApp({super.key, required this.error});
+
+  final String error;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color(0xFF050816),
+        body: Center(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 720),
+            child: Padding(
+              padding: const EdgeInsets.all(24),
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  color: const Color(0xFF0D1220),
+                  border: Border.all(
+                    color: Colors.white.withValues(alpha: 0.12),
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(24),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Waiby could not start',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      const SizedBox(height: 12),
+                      Text(
+                        'The app hit a startup error. Refresh the page after the next deploy if this just changed.',
+                        style: TextStyle(
+                          color: Colors.white.withValues(alpha: 0.8),
+                          fontSize: 16,
+                          height: 1.5,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        'Startup error',
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 13,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      SelectableText(
+                        error,
+                        style: TextStyle(
+                          color: Colors.white.withValues(alpha: 0.9),
+                          fontSize: 14,
+                          height: 1.45,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
